d7cdd693af9471af85132e4199ebfb6f34ff9f99
Nekane Ibarlucea <nknib90@gmail.com>
2022-03-07 17:57:14 +0100

    almost

---
+++ src/components/AddTask.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import axios from "axios"
+
+export default function AddTask(props) {
+  const API_URL = "http://localhost:5005";
+
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+  });
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    // --------- move to its own folder and call createOneProject() --------------
+    axios.post(`${API_URL}/api/tasks`, formData).then((__) => {
+      props.refreshProject();
+      setFormData({
+        title: "",
+        description: "",
+      });
+    });
+    // --------- move to its own folder createOneProject() --------------
+  }
+
+  function handleChange(event) {
+    const key = event.target.name;
+    const value = event.target.value;
+    setFormData((formData) => ({ ...formData, [key]: value }));
+  }
+
+  return (
+    <div className="AddProject">
+      <h3>Add a project</h3>
+
+      <form onSubmit={handleSubmit}>
+        <label>Title:</label>
+        <input type="text" name="title" value={formData.title} onChange={handleChange} />
+
+        <label>Description:</label>
+        <textarea
+          type="text"
+          name="description"
+          value={formData.description}
+          onChange={handleChange}
+        />
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
